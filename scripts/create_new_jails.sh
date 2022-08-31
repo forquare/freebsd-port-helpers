9efe571a96af254bf59e539b4ca7c99802565448
Ben Lavery-Griffiths <ben.lavery@hashbang0.com>
2022-08-31 23:39:15 +0100

    Fix ARCH

--- scripts/create_new_jails.sh
+++ scripts/create_new_jails.sh
@@ -29,20 +29,19 @@ if [ "$#" -eq 2 ]; then
 	if [ "${DIR}" = "releases" ]; then
 		METHOD="ftp"
 	else
-		METHOD="url=ftp://ftp.freebsd.org/pub/FreeBSD/${DIR}/${ARCH}/${ARCH}/${RELEASE}"
+		METHOD="url=ftp://ftp.freebsd.org/pub/FreeBSD/${DIR}/__ARCH__/__ARCH__/${RELEASE}"
 	fi
 else
 	if echo ${RELEASE} | grep -qi 'RELEASE$'; then
 		METHOD="ftp"
 	else
 		DIR='snapshots'
-		METHOD="url=ftp://ftp.freebsd.org/pub/FreeBSD/${DIR}/${ARCH}/${ARCH}/${RELEASE}"
+		METHOD="url=ftp://ftp.freebsd.org/pub/FreeBSD/${DIR}/__ARCH__/__ARCH__/${RELEASE}"
 	fi
 fi
 
-
-
 for ARCH in amd64 i386; do
+	METHOD=$(echo ${METHOD} | sed "s/__ARCH__/${ARCH}/g")
 	poudriere jail -c \
 			-v ${RELEASE} \
 			-a ${ARCH} \
